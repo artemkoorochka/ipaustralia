5ccadb368f257c9bdedc2b6f7bfb486aa803b1b8
artem <artem@koorochka.com>
2022-05-14 17:40:13 +0300

    Search by all pages

--- read_pages.php
+++ read_pages.php
@@ -8,23 +8,18 @@ use PhpQuery\PhpQuery,
 
 $page = 0;
 $trademarks = [];
+$trademarksCount = 0;
 
-/*
 while (true){
     $ipaustralia = new Cipaustralia('https://search.ipaustralia.gov.au/trademarks/search/result?s=7a312c4c-1603-4844-95d6-7c22cca884ec&p=' . $page, new PhpQuery);
     $ipaustralia->setTrademarks('#resultsTable tbody tr');
 
     $trademarks = array_merge($ipaustralia->getTrademarksArray(), $trademarks);
 
+    $trademarksCount += $ipaustralia->getTrademarksCount();
     if($ipaustralia->getTrademarksCount() == 0)
         break;
     $page++;
-    print_r( "Getting results on page: " . $page . "\r\n");
+    print_r( "Read page: #" . $page . "\r\n");
 }
-*/
-
-$ipaustralia = new Cipaustralia('https://search.ipaustralia.gov.au/trademarks/search/result?s=7a312c4c-1603-4844-95d6-7c22cca884ec&p=' . $page, new PhpQuery);
-$ipaustralia->setTrademarks('#resultsTable tbody tr');
-
-$trademarks = array_merge($ipaustralia->getTrademarksArray(), $trademarks);
-#$trademarks = json_encode($trademarks, JSON_PRETTY_PRINT);
+print_r( "Total: " . $trademarksCount . "\r\n");
